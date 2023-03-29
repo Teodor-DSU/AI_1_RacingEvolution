3f434c2c0edf1aefa785f2c6f1f8b244247d5582
a19teosc <60491220+Teodor-DSU@users.noreply.github.com>
2023-03-29 15:46:20 +0200

    Start of player controller

---
+++ a19teosc_evolution/Assets/Scripts/PlayerCarController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCarController : MonoBehaviour
+{
+    [SerializeField] private float Acceleration = .5f;
+    [SerializeField] private float MaxSpeed = 5f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
