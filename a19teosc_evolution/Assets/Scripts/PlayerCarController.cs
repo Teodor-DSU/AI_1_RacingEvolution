5913ffd7423812d1ebd7b181dde1c880bf4a3d71
a19teosc <60491220+Teodor-DSU@users.noreply.github.com>
2023-04-15 18:27:39 +0200

    A functioning car
    Needs a bigger track though

--- a19teosc_evolution/Assets/Scripts/PlayerCarController.cs
+++ a19teosc_evolution/Assets/Scripts/PlayerCarController.cs
@@ -1,19 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerCarController : MonoBehaviour
 {
     [SerializeField] private float drift = 0.8f;
     [SerializeField] private float acceleration = .5f;
-    //[SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float maxSpeed = 5f;
+    [Range(0f, 1f)][SerializeField] private float reverseSpeedMultiplier = 0.5f;
     [SerializeField] private float turnSpeed = 3.5f;
+    [SerializeField] private float turnLimiterForSpeed = 8f;
+    [SerializeField] private float frictionWhenSlowingDown = 3f;
 
     private float _accelerationInput = 0f;
     private float _steeringInput = 0f;
     
     private float _rotationAngle = 0f;
+    private float _velocityLimit = 0f;
 
     private Rigidbody2D _rb;
 
@@ -31,6 +37,25 @@ public class PlayerCarController : MonoBehaviour
 
     private void ApplyEngineForce()
     {
+        _velocityLimit = Vector2.Dot(transform.up, _rb.velocity);
+
+        if (_velocityLimit > maxSpeed && _accelerationInput > 0)
+            return;
+        
+        if (_velocityLimit < -maxSpeed * reverseSpeedMultiplier && _accelerationInput < 0)
+            return;
+        
+        if (_rb.velocity.sqrMagnitude > maxSpeed * maxSpeed && _accelerationInput > 0)
+            return;
+
+        if (_accelerationInput == 0f)
+        {
+            Debug.Log("Current drag: " + _rb.drag);
+            _rb.drag = Mathf.Lerp(_rb.drag, 3.0f, Time.fixedDeltaTime * frictionWhenSlowingDown);
+        }
+        else
+            _rb.drag = 0f;
+        
         Vector2 engineForceVector = transform.up * _accelerationInput * acceleration;
         
         _rb.AddForce(engineForceVector, ForceMode2D.Force);
@@ -38,7 +63,10 @@ public class PlayerCarController : MonoBehaviour
 
     private void Steering()
     {
-        _rotationAngle -= _steeringInput * turnSpeed;
+        float minSpeedToTurn = _rb.velocity.magnitude / turnLimiterForSpeed;
+        minSpeedToTurn = Mathf.Clamp01(minSpeedToTurn);
+        
+        _rotationAngle -= _steeringInput * turnSpeed * minSpeedToTurn;
         
         _rb.MoveRotation(_rotationAngle);
     }
