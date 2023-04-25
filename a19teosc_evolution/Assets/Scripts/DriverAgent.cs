93dfb99d2f5b3434c4769d2daf19e755cb85bb58
a19teosc <60491220+Teodor-DSU@users.noreply.github.com>
2023-04-25 17:49:31 +0200

    Starting with AI

---
+++ a19teosc_evolution/Assets/Scripts/DriverAgent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class DriverAgent : Agent
+{
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        base.CollectObservations(sensor);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        base.OnActionReceived(actions);
+    }
+}
